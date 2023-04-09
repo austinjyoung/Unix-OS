b40dea4253c06dc94b8edd935800e97bd7f2aa1c
Austin Young <austinyoung649@gmail.com>
2023-04-09 13:47:28 -0700

    Weird

--- MyAscii.c
+++ MyAscii.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main() {
-  
+int main() { 
+    // 
     char c;
     printf("Enter a character: ");
     scanf("%c", &c);  
@@ -9,4 +9,3 @@ int main() {
     
     return 0;
 }
-
